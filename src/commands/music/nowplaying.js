2b8b6b4c0f8b8b973e6b327d7e2a58f11b9f51ee
getxd-dev <taminakarl18@gmail.com>
2021-10-16 17:34:30 +0800

    cleaning up the code cuz its kinda messy

--- src/commands/music/nowplaying.js
+++ src/commands/music/nowplaying.js
@@ -28,7 +28,7 @@ module.exports = {
             .setColor(message.guild.me.displayHexColor !== '#000000' ? message.guild.me.displayHexColor : '#00C7FF')
             .setDescription(`[${player.queue.current.isStream ? '◉ LIVE' : `${new Date(player.position).toISOString().slice(11, 19)}`}]` +
             splitBar(musicLength ? Number(musicLength) : 1, nowTime ? Number(nowTime) : 2, 26, '=', '🔘')[0] +
-                `[${player.queue.current.isStream ? '🔴 LIVE' : `${new Date(player.queue.current.duration).toISOString().slice(11, 19)}`}]`)
+            `[${player.queue.current.isStream ? '🔴 LIVE' : `${new Date(player.queue.current.duration).toISOString().slice(11, 19)}`}]`)
         message.channel.send(embed).then(msg => player.set('nowplayingMSG', msg))
 
 
