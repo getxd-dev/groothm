0b828039b81a250899397100c328afcc2e767dd9
getxd-dev <taminakarl18@gmail.com>
2021-10-16 16:14:44 +0800

    spotify playlist/album songs has no limit; modified `queueEnd` message

--- src/lavalink/index.js
+++ src/lavalink/index.js
@@ -164,7 +164,7 @@ class lavalink extends Manager {
             const guild = client.guilds.cache.get(player.guild)
             const noQueueEmbed = new MessageEmbed()
                 .setAuthor("End")
-                .setDescription(`Queue has ended.`)
+                .setDescription(`Seems like queue has ended. Add more by using play command!`)
                 .setColor(guild.me.displayHexColor !== '#000000' ? guild.me.displayHexColor : '#00C7FF')
             if (player.get('nowplaying')) {
                 clearInterval(player.get('nowplaying'));
