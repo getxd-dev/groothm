644b63e0faa7b4f6d0b61661172628f657d87dbc
getxd-dev <taminakarl18@gmail.com>
2021-10-19 20:46:26 +0800

    clean code

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -9,13 +9,13 @@ module.exports = {
     async execute(message, args, client) {
         let player = client.player.players.get(message.guild.id);
         const {channel} = message.member.voice;
-        if (!channel) return message.channel.send(handler.normalEmbed('You\'re not in a voice channel'))
+        if (!channel) return message.channel.send(handler.normalEmbed("You're not on a voice channel."))
         const permissions = message.member.voice.channel.permissionsFor(message.client.user);
-        if (!permissions.has('CONNECT')) return message.channel.send(handler.normalEmbed('I don\'t have \`CONNECT\` permission'))
-        if (!permissions.has('SPEAK')) return message.channel.send(handler.normalEmbed('I don\'t have \`SPEAK\` permission'))
+        if (!permissions.has('CONNECT')) return message.channel.send(handler.normalEmbed("I don't have CONNECT permission"))
+        if (!permissions.has('SPEAK')) return message.channel.send(handler.normalEmbed("I don't have SPEAK permission"))
 
-        if (player && (channel.id !== player?.voiceChannel)) return message.channel.send(handler.normalEmbed('You\'re not in my voice channel'))
-        if (!args[0]) return message.channel.send(handler.noArgument(client, this.name, ['play < youtube url | query | youtube playlist | spotify track | spotify playlist | spotify album | twitch >']))
+        if (player && (channel.id !== player?.voiceChannel)) return message.channel.send(handler.normalEmbed("You're not in my voice channel"))
+        if (!args[0]) return message.channel.send(handler.noArgument(client, this.name, ['play < YouTube URL | Query | YouTube Playlist | Spotify Track | Spotify Playlist | Spotify Album | Twitch >']))
         if (!player) {
             player = client.player.create({
                 guild: message.guild.id,
