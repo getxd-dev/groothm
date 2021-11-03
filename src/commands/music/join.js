adea9bda6f3b0ddbddf670f5259fa17bc91e09a9
getxd-dev <taminakarl18@gmail.com>
2021-11-03 09:29:51 +0800

    fix spelling errors, clean up

--- src/commands/music/join.js
+++ src/commands/music/join.js
@@ -20,6 +20,6 @@ module.exports = {
             player.connect()
             message.react('✋').catch(() => { })
         }
-        return message.channel.send(handler.normalEmbed('\'m in another channel'))
+        return message.channel.send(handler.normalEmbed("I'm in another channel"))
     }
 }
